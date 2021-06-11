d7f342d9f1bbc12e6a017f864ac8df9a2b3da6bb
GeovanyOjeda14 <geovanyojeda14@gmail.com>
2021-06-11 17:41:42 -0500

    ver mas y acceder en inicion pagina

---
+++ scripts.d3ec66cb4a0825517c79.js
@@ -0,0 +1,19 @@
+function classToggle() {
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('');
+  }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    //var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+;
+//# sourceMappingURL=scripts.d3ec66cb4a0825517c79.js.map
